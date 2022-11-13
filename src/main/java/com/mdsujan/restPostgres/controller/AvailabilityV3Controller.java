100d1d52534eda664b132845345304063c5e5f80
mds9903 <mohammed.sujan@nextuple.com>
2022-11-13 18:44:27 +0530

    added v3/availability get; working as expected

--- src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV3Controller.java
+++ src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV3Controller.java
@@ -10,6 +10,7 @@ import com.mdsujan.restPostgres.repository.ItemRepository;
 import com.mdsujan.restPostgres.repository.SupplyRepository;
 import com.mdsujan.restPostgres.repository.ThresholdRepository;
 import com.mdsujan.restPostgres.response.AvailabilityV3Response;
+import com.mdsujan.restPostgres.response.ThresholdResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,7 +22,9 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
+
 @RestController
 @RequestMapping("/v3/availability")
 public class AvailabilityV3Controller {
@@ -38,53 +41,51 @@ public class AvailabilityV3Controller {
     ThresholdRepository thresholdRepository;
 
     @GetMapping("/{itemId}/{locationId}")
-//    public AvailabilityV3Response getAvailabilityStockLevel(@PathVariable Long itemId, @PathVariable Long locationId) {
-    public String getAvailabilityStockLevel(@PathVariable Long itemId, @PathVariable Long locationId) {
+    public AvailabilityV3Response getAvailabilityStockLevel(@PathVariable Long itemId, @PathVariable Long locationId) {
         Properties prop = new Properties();
         String fileName = "/home/sujan/Documents/restPostgres/src/main/java/com/mdsujan/restPostgres/availability.config";
         try (FileInputStream fis = new FileInputStream(fileName)) {
             prop.load(fis);
+            // get the availabilityQty for given itemId and locationId
+            List<Supply> supplyList = supplyRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+            List<Demand> demandList = demandRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+            // for supplies
+            Long supplyQty = 0L;
+            List<String> supplyTypes = List.of(prop.getProperty("supplies").split(","));
+            for (String supplyType : supplyTypes) {
+                supplyQty += supplyList
+                        .stream()
+                        .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.valueOf(supplyType))
+                        .map(Supply::getQty)
+                        .reduce(0L, (a, b) -> a + b);
+            }
+            Long demandQty = 0L;
+            List<String> demandTypes = List.of(prop.getProperty("demands").split(","));
+            for (String demandType : demandTypes) {
+                demandQty += demandList.stream().filter(demand -> demand.getDemandType() == AllowedDemandTypes.valueOf(demandType)).map(Demand::getDemandQty).reduce(0L, (a, b) -> a + b);
+            }
+            Long availableQty = supplyQty + demandQty;
+            /*
+             *
+             *  if availabilityQty < minThreshold then red
+             *  if availability > maxThreshold then green
+             *  else yellow (availabilityQty >= min and <= max)
+             *
+             * */
+            Threshold threshold = thresholdRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+            System.out.println(threshold);
+            String stockLevel = "yellow";
+            if (availableQty < threshold.getMinThreshold()) {
+                stockLevel = "Red";
+            } else if (availableQty > threshold.getMaxThreshold()) {
+                stockLevel = "Green";
+            }
+            return new AvailabilityV3Response(itemId, locationId, availableQty, stockLevel);
         } catch (FileNotFoundException fileNotFoundException) {
             System.out.println("file not found");
         } catch (IOException e) {
             System.out.println("exception occurred: " + e.getMessage());
         }
-        System.out.println("supplies " + prop.getProperty("supplies"));
-        System.out.println("demands " + prop.getProperty("demands"));
-        System.out.println("locations.exclude " + prop.getProperty("locations.exclude"));
-        return prop.getProperty("supplies")
-                + "\n" + prop.getProperty("demands")
-                + "\n" + prop.getProperty("locations.exclude");
-//        // get the availabilityQty for given itemId and locationId
-//        Long onhandQty = supplyRepository.findByItemItemIdAndLocationLocationId(itemId, locationId)
-//                .stream()
-//                .filter(supply -> supply.getSupplyType() == availabilityConfig)
-//                .map(Supply::getQty)
-//                .reduce(0L, (a, b) -> a + b);
-//        // get the hardPromised qty for the demand of the given item and location
-//        Long hardPromisedQty = demandRepository.findByItemItemIdAndLocationLocationId(itemId, locationId)
-//                .stream()
-//                .filter(demand -> demand.getDemandType() == AllowedDemandTypes.HARD_PROMISED)
-//                .map(Demand::getDemandQty)
-//                .reduce(0L, (a, b) -> a + b);
-//
-//        Long availableQty = onhandQty + hardPromisedQty;
-//        /*
-//         *
-//         *  if availabilityQty < minThreshold then red
-//         *  if availability > maxThreshold then green
-//         *  else yellow (availabilityQty >= min and <= max)
-//         *
-//         * */
-//        Threshold threshold = thresholdRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-//        System.out.println(threshold);
-//        String stockLevel = "yellow";
-//        if (availableQty < threshold.getMinThreshold()) {
-//            stockLevel = "Red";
-//        } else if (availableQty > threshold.getMaxThreshold()) {
-//            stockLevel = "Green";
-//        }
-
-//        return new AvailabilityV3Response(itemId, locationId, availableQty, stockLevel);
+        return null;
     }
 }
