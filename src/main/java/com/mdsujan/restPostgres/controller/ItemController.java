9bb39ee752ef6388d63d4fac63f558841ef59a30
mds9903 <mohammed.sujan@nextuple.com>
2022-11-15 09:44:50 +0530

    added logging for controller pkg; set up for item controller done

--- src/main/java/com/mdsujan/restPostgres/controller/ItemController.java
+++ src/main/java/com/mdsujan/restPostgres/controller/ItemController.java
@@ -18,7 +18,7 @@ import java.util.List;
 @RequestMapping("/items")
 public class ItemController {
 
-    Logger logger = LoggerFactory.getLogger(ItemListener.class);
+    Logger logger = LoggerFactory.getLogger(ItemController.class);
 
     @Autowired
     ItemService itemService;
@@ -26,43 +26,62 @@ public class ItemController {
     @GetMapping("/") // return all items
     public List<ItemResponse> getAllItems() {
 
-        logger.error("Logger Error");
-        logger.warn("Logger Warn");
-        logger.info("Logger Info");
-        logger.debug("Logger Debug");
-        logger.trace("Logger Trace");
+//        logger.error("Logger Error");
+//        logger.warn("Logger Warn");
+//        logger.info("Logger Info");
+//        logger.debug("Logger Debug");
+//        logger.trace("Logger Trace");
+
 
         List<Item> itemList = itemService.getAllItems();
         List<ItemResponse> itemResponseList = new ArrayList<>();
 
         // convert each Item obj to ItemResponse
         itemList.forEach(item -> itemResponseList.add(new ItemResponse(item)));
+        logger.info("Response: " + itemResponseList); // log the response being sent
         return itemResponseList;
+
+
     }
 
     @GetMapping("/{itemId}") // return the details of specific itemId
     public ItemResponse getItem(@PathVariable Long itemId) {
-        return new ItemResponse(itemService.getItemById(itemId));
+        ItemResponse itemResponse = new ItemResponse(itemService.getItemById(itemId));
+        logger.info("Response: " + itemResponse);
+
+        return itemResponse;
     }
 
     @PostMapping("/") // create an item in the table
     public ItemResponse createItem(@RequestBody CreateItemRequest createItemRequest) {
+        logger.info("InQueryRequest: "+createItemRequest);
         Item newItem = itemService.createItem(createItemRequest);
-        return new ItemResponse(newItem);
+        ItemResponse itemResponse = new ItemResponse(newItem);
+        logger.info("Response: "+itemResponse);
+        return itemResponse;
     }
 
     @DeleteMapping("/{itemId}") // delete a specific item
     public String deleteItem(@PathVariable Long itemId) {
-        return itemService.deleteItemById(itemId);
+
+        String response = itemService.deleteItemById(itemId);
+        logger.info("Response: "+response);
+        return response;
     }
 
     @PutMapping("/{itemId}")
     public ItemResponse updateItemPut(@PathVariable Long itemId, @RequestBody UpdateItemRequest updateItemRequest) {
-        return new ItemResponse(itemService.updateItemByIdPut(itemId, updateItemRequest));
+        logger.info("InQueryRequest: "+updateItemRequest);
+        ItemResponse itemResponse = new ItemResponse(itemService.updateItemByIdPut(itemId, updateItemRequest));
+        logger.info("Response: "+itemResponse);
+        return itemResponse;
     }
 
     @PatchMapping("/{itemId}")
-    public ItemResponse updateItemPatch(@PathVariable Long itemId, @RequestBody UpdateItemRequest updateItemRequest){
-        return new ItemResponse(itemService.updateItemByIdPatch(itemId, updateItemRequest));
+    public ItemResponse updateItemPatch(@PathVariable Long itemId, @RequestBody UpdateItemRequest updateItemRequest) {
+        logger.info("InQueryRequest: "+updateItemRequest);
+        ItemResponse itemResponse = new ItemResponse(itemService.updateItemByIdPatch(itemId, updateItemRequest));
+        logger.info("Response: "+itemResponse);
+        return itemResponse;
     }
 }
