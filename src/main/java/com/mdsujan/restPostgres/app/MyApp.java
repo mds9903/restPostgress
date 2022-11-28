9cc5697c758f0166b4139061d8d0e1967d05a36f
mds9903 <mohammed.sujan@nextuple.com>
2022-11-28 13:44:38 +0530

    issue checkpoint; jdbc template issues in item controller test

--- src/main/java/com/mdsujan/restPostgres/app/MyApp.java
+++ src/main/java/com/mdsujan/restPostgres/app/MyApp.java
@@ -3,10 +3,12 @@ package com.mdsujan.restPostgres.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = SqlInitializationAutoConfiguration.class)
 @ComponentScan("com.mdsujan.restPostgres")
 @EntityScan("com.mdsujan.restPostgres.entity")
 @EnableJpaRepositories("com.mdsujan.restPostgres.repository")
---
+++ src/test/java/com/mdsujan/restPostgres/ItemControllerTest.java
@@ -0,0 +1,74 @@
+package com.mdsujan.restPostgres;
+
+import com.mdsujan.restPostgres.controller.ItemController;
+import com.mdsujan.restPostgres.repository.ItemRepository;
+import com.mdsujan.restPostgres.service.ItemService;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.ModelAndViewAssert;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.web.servlet.ModelAndView;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//@TestPropertySource("/application.properties")
+//@ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(classes = ItemController.class)
+public class ItemControllerTest {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    private MockMvc mockMvc; // our mock
+
+    //    @Mock
+    @MockBean
+    private ItemService itemService;
+
+
+    @BeforeEach
+    public void addTestDbData() {
+//        jdbcTemplate = new JdbcTemplate();
+        jdbcTemplate.execute("insert into items " +
+                "values(123456, 'testDesc', 'testCategory', 'testType'," +
+                " 'testStatus', 99.99, false, false, false)");
+    }
+
+    @AfterEach
+    public void cleanTestDbData() {
+        jdbcTemplate.execute("delete from items where itemId=123456");
+    }
+
+    @Test
+    public void getItemHttpRequestTest() throws Throwable {
+        // perform the web request
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                // define the expectations
+                .andExpect(status().isOk()).andReturn();
+
+        ModelAndView mav = mvcResult.getModelAndView(); // get the result model and view
+
+//        assert mav != null;
+        ModelAndViewAssert.assertViewName(mav, itemService.getAllItems().toString());
+    }
+}
