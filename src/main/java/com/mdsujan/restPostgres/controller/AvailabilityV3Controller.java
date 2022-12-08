2a958ed84fda1c8497f5fb6b056867a201704b09
mds9903 <mohammed.sujan@nextuple.com>
2022-12-08 15:13:23 +0530

    minor code cleanup; all unit tests completed for all controllers and services

--- src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV3Controller.java
+++ src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV3Controller.java
@@ -38,69 +38,13 @@ public class AvailabilityV3Controller {
     @Autowired
     ThresholdRepository thresholdRepository;
 
-//    @Value("#${V3Availability.config}")
-//    private Map<String, String> configMap;
 
-//    @Autowired
+
     @Value("${supplyTypes}")
     private String supplyTypes;
 
-//    @Autowired
     @Value("${demandTypes}")
     private String demandTypes;
-
-//    @Value("${locations.exclude}")
-//    private String locationsExclude;
-
-
-//    @GetMapping("/{itemId}/{locationId}")
-//    public AvailabilityV3Response getAvailabilityStockLevel(@PathVariable Long itemId, @PathVariable Long locationId) {
-//        Properties prop = new Properties();
-//        String fileName = "/home/sujan/Documents/restPostgres/src/main/java/com/mdsujan/restPostgres/availability.config";
-//        try (FileInputStream fis = new FileInputStream(fileName)) {
-//            prop.load(fis);
-//            // get the availabilityQty for given itemId and locationId
-//            List<Supply> supplyList = supplyRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-//            List<Demand> demandList = demandRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-//            // for supplies
-//            Long supplyQty = 0L;
-//            List<String> supplyTypes = List.of(prop.getProperty("supplies").split(","));
-//            for (String supplyType : supplyTypes) {
-//                supplyQty += supplyList
-//                        .stream()
-//                        .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.valueOf(supplyType))
-//                        .map(Supply::getSupplyQty)
-//                        .reduce(0L, Long::sum);
-//            }
-//            Long demandQty = 0L;
-//            List<String> demandTypes = List.of(prop.getProperty("demands").split(","));
-//            for (String demandType : demandTypes) {
-//                demandQty += demandList.stream().filter(demand -> demand.getDemandType() == AllowedDemandTypes.valueOf(demandType)).map(Demand::getDemandQty).reduce(0L, (a, b) -> a + b);
-//            }
-//            Long availableQty = supplyQty + demandQty;
-//            /*
-//             *
-//             *  if availabilityQty < minThreshold then red
-//             *  if availability > maxThreshold then green
-//             *  else yellow (availabilityQty >= min and <= max)
-//             *
-//             * */
-//            Threshold threshold = thresholdRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-//            System.out.println(threshold);
-//            String stockLevel = "yellow";
-//            if (availableQty < threshold.getMinThreshold()) {
-//                stockLevel = "Red";
-//            } else if (availableQty > threshold.getMaxThreshold()) {
-//                stockLevel = "Green";
-//            }
-//            return new AvailabilityV3Response(itemId, locationId, availableQty, stockLevel);
-//        } catch (FileNotFoundException fileNotFoundException) {
-//            System.out.println("file not found");
-//        } catch (IOException e) {
-//            System.out.println("exception occurred: " + e.getMessage());
-//        }
-//        return null;
-//    }
 
     @GetMapping("/{itemId}/{locationId}")
     public AvailabilityV3Response getAvailabilityStockLevel(@PathVariable Long itemId, @PathVariable Long locationId) {
