3c15fe16e15d8eb86164e8710242c049a897826c
mds9903 <mohammed.sujan@nextuple.com>
2022-12-09 10:15:41 +0530

    added enable caching to MyApp.java

--- src/main/java/com/mdsujan/restPostgres/app/MyApp.java
+++ src/main/java/com/mdsujan/restPostgres/app/MyApp.java
@@ -4,14 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.sql.init.SqlInitializationAutoConfiguration;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = SqlInitializationAutoConfiguration.class)
 @ComponentScan("com.mdsujan.restPostgres")
 @EntityScan("com.mdsujan.restPostgres.entity")
 @EnableJpaRepositories("com.mdsujan.restPostgres.repository")
+@EnableCaching
 public class MyApp {
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
