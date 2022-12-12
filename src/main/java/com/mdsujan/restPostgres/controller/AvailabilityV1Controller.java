84a8c2a4060f283822619b548d364242a871ebf5
mds9903 <mohammed.sujan@nextuple.com>
2022-12-12 13:59:53 +0530

    combined availability controller works but old ones need to be deleted or commented

--- src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV1Controller.java
+++ src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV1Controller.java
@@ -1,97 +1,97 @@
-package com.mdsujan.restPostgres.controller;
-
-import com.mdsujan.restPostgres.entity.Demand;
-import com.mdsujan.restPostgres.entity.Supply;
-import com.mdsujan.restPostgres.enums.AllowedDemandTypes;
-import com.mdsujan.restPostgres.enums.AllowedSupplyTypes;
-import com.mdsujan.restPostgres.exceptionHandling.ResourceNotFoundException;
-import com.mdsujan.restPostgres.repository.DemandRepository;
-import com.mdsujan.restPostgres.repository.SupplyRepository;
-import com.mdsujan.restPostgres.response.AvailabilityV1Response;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/v1/availability")
-public class AvailabilityV1Controller {
-    @Autowired
-    SupplyRepository supplyRepository;
-
-    @Autowired
-    DemandRepository demandRepository;
-
-    @GetMapping("/{itemId}/{locationId}") // get availability qty of item at a given location
-    public AvailabilityV1Response getAvailabilityQtyByItemAndLocation(@PathVariable Long itemId, @PathVariable Long locationId) throws Throwable {
-        /*sample response
-         * {
-         *   "itemId":123,
-         *   "locationId": 456,
-         *   "availableQty": 5
-         * }
-         *
-         * availableQty = supplyQty - demandQty
-         * Note: consider only ONHAND supply and HARD_PROMISED demand
-         * */
-        // get the onhand qty for the supply of given item and location
-        List<Supply> supplyList = supplyRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-        List<Demand> demandList = demandRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-        if(supplyList == null || demandList == null){
-            throw new ResourceNotFoundException("no supplies for this itemId and locationId; please enter correct itemId and locationId");
-        }
-        Long onhandQty = supplyList
-                .stream()
-                .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.ONHAND)
-                .map(Supply::getSupplyId)
-                .reduce(0L, Long::sum);
-        // get the hardPromised qty for the demand of the given item and location
-        Long hardPromisedQty = demandList
-                .stream()
-                .filter(demand -> demand.getDemandType() == AllowedDemandTypes.HARD_PROMISED)
-                .map(Demand::getDemandQty)
-                .reduce(0L, Long::sum);
-
-        Long availableQty = onhandQty + hardPromisedQty;
-
-        return new AvailabilityV1Response(itemId, locationId, availableQty);
-    }
-
-    @GetMapping("/{itemId}") // get availability qty of item in all locations (total)
-    public AvailabilityV1Response getAllLocationAvailabilityQty(@PathVariable Long itemId) throws Throwable{
-        /*sample response
-         * {
-         *   "itemId":123,
-         *   "locationId": "NETWORK",
-         *   "availableQty": 5
-         * }
-         *
-         * availableQty = supplyQty - demandQty
-         * Note: consider only ONHAND supply and HARD_PROMISED demand
-         * */
-        List<Supply> supplyList = supplyRepository.findByItemItemId(itemId);
-        List<Demand> demandList = demandRepository.findByItemItemId(itemId);
-        if(supplyList == null || demandList == null){
-            throw new ResourceNotFoundException("no supplies and/or demands for this itemId and locationId; please enter correct itemId and locationId");
-        }
-        // get the onhand qty for the supply of given item on all locations
-        Long onhandQty = supplyList
-                .stream()
-                .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.ONHAND)
-                .map(Supply::getSupplyId)
-                .reduce(0L, Long::sum);
-        // get the hardPromised qty for the demand of the given item on all locations
-        Long hardPromisedQty = demandList
-                .stream()
-                .filter(demand -> demand.getDemandType() == AllowedDemandTypes.HARD_PROMISED)
-                .map(Demand::getDemandQty)
-                .reduce(0L, Long::sum);
-
-        Long availableQty = onhandQty + hardPromisedQty;
-
-        return new AvailabilityV1Response(itemId, "NETWORK", availableQty);
-    }
-}
+//package com.mdsujan.restPostgres.controller;
+//
+//import com.mdsujan.restPostgres.entity.Demand;
+//import com.mdsujan.restPostgres.entity.Supply;
+//import com.mdsujan.restPostgres.enums.AllowedDemandTypes;
+//import com.mdsujan.restPostgres.enums.AllowedSupplyTypes;
+//import com.mdsujan.restPostgres.exceptionHandling.ResourceNotFoundException;
+//import com.mdsujan.restPostgres.repository.DemandRepository;
+//import com.mdsujan.restPostgres.repository.SupplyRepository;
+//import com.mdsujan.restPostgres.response.AvailabilityV1Response;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.util.List;
+//
+//@RestController
+//@RequestMapping("/v1/availability")
+//public class AvailabilityV1Controller {
+//    @Autowired
+//    SupplyRepository supplyRepository;
+//
+//    @Autowired
+//    DemandRepository demandRepository;
+//
+//    @GetMapping("/{itemId}/{locationId}") // get availability qty of item at a given location
+//    public AvailabilityV1Response getAvailabilityQtyByItemAndLocation(@PathVariable Long itemId, @PathVariable Long locationId) throws Throwable {
+//        /*sample response
+//         * {
+//         *   "itemId":123,
+//         *   "locationId": 456,
+//         *   "availableQty": 5
+//         * }
+//         *
+//         * availableQty = supplyQty - demandQty
+//         * Note: consider only ONHAND supply and HARD_PROMISED demand
+//         * */
+//        // get the onhand qty for the supply of given item and location
+//        List<Supply> supplyList = supplyRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+//        List<Demand> demandList = demandRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+//        if(supplyList == null || demandList == null){
+//            throw new ResourceNotFoundException("no supplies for this itemId and locationId; please enter correct itemId and locationId");
+//        }
+//        Long onhandQty = supplyList
+//                .stream()
+//                .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.ONHAND)
+//                .map(Supply::getSupplyId)
+//                .reduce(0L, Long::sum);
+//        // get the hardPromised qty for the demand of the given item and location
+//        Long hardPromisedQty = demandList
+//                .stream()
+//                .filter(demand -> demand.getDemandType() == AllowedDemandTypes.HARD_PROMISED)
+//                .map(Demand::getDemandQty)
+//                .reduce(0L, Long::sum);
+//
+//        Long availableQty = onhandQty + hardPromisedQty;
+//
+//        return new AvailabilityV1Response(itemId, locationId, availableQty);
+//    }
+//
+//    @GetMapping("/{itemId}") // get availability qty of item in all locations (total)
+//    public AvailabilityV1Response getAllLocationAvailabilityQty(@PathVariable Long itemId) throws Throwable{
+//        /*sample response
+//         * {
+//         *   "itemId":123,
+//         *   "locationId": "NETWORK",
+//         *   "availableQty": 5
+//         * }
+//         *
+//         * availableQty = supplyQty - demandQty
+//         * Note: consider only ONHAND supply and HARD_PROMISED demand
+//         * */
+//        List<Supply> supplyList = supplyRepository.findByItemItemId(itemId);
+//        List<Demand> demandList = demandRepository.findByItemItemId(itemId);
+//        if(supplyList == null || demandList == null){
+//            throw new ResourceNotFoundException("no supplies and/or demands for this itemId and locationId; please enter correct itemId and locationId");
+//        }
+//        // get the onhand qty for the supply of given item on all locations
+//        Long onhandQty = supplyList
+//                .stream()
+//                .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.ONHAND)
+//                .map(Supply::getSupplyId)
+//                .reduce(0L, Long::sum);
+//        // get the hardPromised qty for the demand of the given item on all locations
+//        Long hardPromisedQty = demandList
+//                .stream()
+//                .filter(demand -> demand.getDemandType() == AllowedDemandTypes.HARD_PROMISED)
+//                .map(Demand::getDemandQty)
+//                .reduce(0L, Long::sum);
+//
+//        Long availableQty = onhandQty + hardPromisedQty;
+//
+//        return new AvailabilityV1Response(itemId, "NETWORK", availableQty);
+//    }
+//}
--- src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV3Controller.java
+++ src/main/java/com/mdsujan/restPostgres/controller/AvailabilityV3Controller.java
@@ -1,93 +1,93 @@
-package com.mdsujan.restPostgres.controller;
-
-import com.mdsujan.restPostgres.entity.Demand;
-import com.mdsujan.restPostgres.entity.Supply;
-import com.mdsujan.restPostgres.entity.Threshold;
-import com.mdsujan.restPostgres.enums.AllowedDemandTypes;
-import com.mdsujan.restPostgres.enums.AllowedSupplyTypes;
-import com.mdsujan.restPostgres.repository.DemandRepository;
-import com.mdsujan.restPostgres.repository.ItemRepository;
-import com.mdsujan.restPostgres.repository.SupplyRepository;
-import com.mdsujan.restPostgres.repository.ThresholdRepository;
-import com.mdsujan.restPostgres.response.AvailabilityV3Response;
-import com.mdsujan.restPostgres.response.ThresholdResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.List;
-import java.util.Map;
-import java.util.Properties;
-
-@RestController
-@RequestMapping("/v3/availability")
-public class AvailabilityV3Controller {
-    @Autowired
-    SupplyRepository supplyRepository;
-
-    @Autowired
-    DemandRepository demandRepository;
-
-    @Autowired
-    ThresholdRepository thresholdRepository;
-
-
-
-    @Value("${supplyTypes}")
-    private String supplyTypes;
-
-    @Value("${demandTypes}")
-    private String demandTypes;
-
-    @GetMapping("/{itemId}/{locationId}")
-    public AvailabilityV3Response getAvailabilityStockLevel(@PathVariable Long itemId, @PathVariable Long locationId) {
-        // get the availabilityQty for given itemId and locationId
-        List<Supply> supplyList = supplyRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-        Long supplyQty = 0L;
-        List<String> supplyTypeList = List.of(supplyTypes); // get the supply types from the config
-
-        List<Demand> demandList = demandRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-        Long demandQty = 0L;
-        List<String> demandTypeList = List.of(demandTypes);  // get the supply types from the config
-
-        System.out.printf("supplyTypeList : "+supplyTypeList);
-        System.out.printf("demandTypeList : "+demandTypeList);
-
-        for (String supplyType : supplyTypeList) {
-            supplyQty += supplyList.stream()
-                    .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.valueOf(supplyType))
-                    .map(Supply::getSupplyQty)
-                    .reduce(0L, Long::sum);
-        }
-        for (String demandType : demandTypeList) {
-            demandQty += demandList.stream()
-                    .filter(demand -> demand.getDemandType() == AllowedDemandTypes.valueOf(demandType))
-                    .map(Demand::getDemandQty)
-                    .reduce(0L, (a, b) -> a + b);
-        }
-        Long availableQty = supplyQty + demandQty;
-        /*
-         *
-         *  if availabilityQty < minThreshold then red
-         *  if availability > maxThreshold then green
-         *  else yellow (availabilityQty >= min and <= max)
-         *
-         * */
-        Threshold threshold = thresholdRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
-        System.out.println(threshold);
-        String stockLevel = "yellow";
-        if (availableQty < threshold.getMinThreshold()) {
-            stockLevel = "Red";
-        } else if (availableQty > threshold.getMaxThreshold()) {
-            stockLevel = "Green";
-        }
-        return new AvailabilityV3Response(itemId, locationId, availableQty, stockLevel);
-    }
-}
+//package com.mdsujan.restPostgres.controller;
+//
+//import com.mdsujan.restPostgres.entity.Demand;
+//import com.mdsujan.restPostgres.entity.Supply;
+//import com.mdsujan.restPostgres.entity.Threshold;
+//import com.mdsujan.restPostgres.enums.AllowedDemandTypes;
+//import com.mdsujan.restPostgres.enums.AllowedSupplyTypes;
+//import com.mdsujan.restPostgres.repository.DemandRepository;
+//import com.mdsujan.restPostgres.repository.ItemRepository;
+//import com.mdsujan.restPostgres.repository.SupplyRepository;
+//import com.mdsujan.restPostgres.repository.ThresholdRepository;
+//import com.mdsujan.restPostgres.response.AvailabilityV3Response;
+//import com.mdsujan.restPostgres.response.ThresholdResponse;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import java.io.FileInputStream;
+//import java.io.FileNotFoundException;
+//import java.io.FileReader;
+//import java.io.IOException;
+//import java.util.List;
+//import java.util.Map;
+//import java.util.Properties;
+//
+//@RestController
+//@RequestMapping("/v3/availability")
+//public class AvailabilityV3Controller {
+//    @Autowired
+//    SupplyRepository supplyRepository;
+//
+//    @Autowired
+//    DemandRepository demandRepository;
+//
+//    @Autowired
+//    ThresholdRepository thresholdRepository;
+//
+//
+//
+//    @Value("${supplyTypes}")
+//    private String supplyTypes;
+//
+//    @Value("${demandTypes}")
+//    private String demandTypes;
+//
+//    @GetMapping("/{itemId}/{locationId}")
+//    public AvailabilityV3Response getAvailabilityStockLevel(@PathVariable Long itemId, @PathVariable Long locationId) {
+//        // get the availabilityQty for given itemId and locationId
+//        List<Supply> supplyList = supplyRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+//        Long supplyQty = 0L;
+//        List<String> supplyTypeList = List.of(supplyTypes); // get the supply types from the config
+//
+//        List<Demand> demandList = demandRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+//        Long demandQty = 0L;
+//        List<String> demandTypeList = List.of(demandTypes);  // get the supply types from the config
+//
+//        System.out.printf("supplyTypeList : "+supplyTypeList);
+//        System.out.printf("demandTypeList : "+demandTypeList);
+//
+//        for (String supplyType : supplyTypeList) {
+//            supplyQty += supplyList.stream()
+//                    .filter(supply -> supply.getSupplyType() == AllowedSupplyTypes.valueOf(supplyType))
+//                    .map(Supply::getSupplyQty)
+//                    .reduce(0L, Long::sum);
+//        }
+//        for (String demandType : demandTypeList) {
+//            demandQty += demandList.stream()
+//                    .filter(demand -> demand.getDemandType() == AllowedDemandTypes.valueOf(demandType))
+//                    .map(Demand::getDemandQty)
+//                    .reduce(0L, (a, b) -> a + b);
+//        }
+//        Long availableQty = supplyQty + demandQty;
+//        /*
+//         *
+//         *  if availabilityQty < minThreshold then red
+//         *  if availability > maxThreshold then green
+//         *  else yellow (availabilityQty >= min and <= max)
+//         *
+//         * */
+//        Threshold threshold = thresholdRepository.findByItemItemIdAndLocationLocationId(itemId, locationId);
+//        System.out.println(threshold);
+//        String stockLevel = "yellow";
+//        if (availableQty < threshold.getMinThreshold()) {
+//            stockLevel = "Red";
+//        } else if (availableQty > threshold.getMaxThreshold()) {
+//            stockLevel = "Green";
+//        }
+//        return new AvailabilityV3Response(itemId, locationId, availableQty, stockLevel);
+//    }
+//}
