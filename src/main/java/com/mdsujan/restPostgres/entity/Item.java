05cc12f759b871d3dbef7775cb0b609052cc44c8
mds9903 <mohammed.sujan@nextuple.com>
2022-12-15 22:07:17 +0530

    added caching using redis; note: the object to be cached should be Serializable else will cause exceptions

--- src/main/java/com/mdsujan/restPostgres/entity/Item.java
+++ src/main/java/com/mdsujan/restPostgres/entity/Item.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.NotNull;
+import java.io.Serializable;
 
 @Entity
 @Getter
@@ -14,7 +15,7 @@ import javax.validation.constraints.NotNull;
 @Table(name = "item", schema = "public")
 @AllArgsConstructor
 @NoArgsConstructor
-public class Item {
+public class Item implements Serializable {
 
     @Id
     @Column(name = "item_id")
