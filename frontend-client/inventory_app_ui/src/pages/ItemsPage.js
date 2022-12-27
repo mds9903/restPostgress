459779ce4adb14ae8d51115470834465f1a3813a
mds9903 <mohammed.sujan@nextuple.com>
2022-12-27 21:50:42 +0530

    added paginated url

--- frontend-client/inventory_app_ui/src/pages/ItemsPage.js
+++ frontend-client/inventory_app_ui/src/pages/ItemsPage.js
@@ -1,12 +1,15 @@
 import { useState, useEffect } from "react";
 import MyTable from "../components/MyTable";
-import { Button, Container, Row, Col } from "react-bootstrap";
+import { Button, Container, Row, Col, Form } from "react-bootstrap";
 
 import axios from "axios";
 
 const getAllUrl = "http://localhost:8088/items/";
+const getAllPaginatedUr = "http://localhost:8088/items/paginated";
 
 function ItemsPage() {
+  const [pageSize, setPageSize] = useState(1);
+  const [pageNum, setPageNum] = useState(3);
   const [isDataLoaded, setDataLoaded] = useState(false);
 
   const [tableData, setTableData] = useState(null);
@@ -14,14 +17,18 @@ function ItemsPage() {
   const [shouldReload, setShouldReload] = useState(false);
 
   useEffect(() => {
-    axios.get(getAllUrl).then((response) => {
-      console.log("getting data");
-      console.log("response.data:\n", response.data);
-      setDataLoaded(true);
-      setTableData(response.data);
-      // console.log(tableData);
-      setShouldReload(false);
-    });
+    axios
+      .get(getAllPaginatedUr, {
+        params: { pageSize: pageSize, pageNum: pageNum },
+      })
+      .then((response) => {
+        console.log("getting data");
+        console.log("response.data:\n", response.data);
+        setDataLoaded(true);
+        setTableData(response.data);
+        // console.log(tableData);
+        setShouldReload(false);
+      });
   }, [shouldReload]);
 
   const reloadTable = () => {
@@ -51,6 +58,12 @@ function ItemsPage() {
         <Row>
           <Col>
             <MyTable tableData={tableData} />
+            <Form.Select>
+              <option>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </Form.Select>
           </Col>
         </Row>
       </Container>
