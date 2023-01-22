7e9322b6becd896d4dffb4c570ed7aa927233963
mds9903 <mohammed.sujan@nextuple.com>
2023-01-23 01:52:55 +0530

    MyPieChart cleanup

--- frontend-client/inventory_app_ui/src/components/MyPieChart.js
+++ frontend-client/inventory_app_ui/src/components/MyPieChart.js
@@ -3,36 +3,41 @@ import { Chart as ChartJS } from "chart.js";
 import { Colors } from "chart.js/auto";
 import ChartDataLabels from "chartjs-plugin-datalabels";
 
-ChartJS.register(Colors);
-ChartJS.register(ChartDataLabels);
-const onClickHandler = (e) =>
-  console.log(JSON.stringify(e.chart.$context.chart.tooltip.body[0].lines[0]));
-const options = {
-  onClick: onClickHandler,
-  maintainAspectRatio: false,
-  plugins: {
-    // tooltip: {
-    //   // Tooltip will only receive click events
-    //   events: ["click"],
-    // },
-    datalabels: {
-      // This code is used to display data values
-      anchor: "center",
-      align: "start",
-      color: "black",
-      font: {
-        size: 20,
+export default function MyPieChart(props) {
+  ChartJS.register(Colors);
+  ChartJS.register(ChartDataLabels);
+  const onClickHandler = (e) => {
+    console.log("MyPieChart onClick handler");
+    console.log("event: ", e);
+    console.log(
+      JSON.stringify(e.chart.$context.chart.tooltip.body[0].lines[0])
+    );
+  };
+  const options = {
+    onClick: onClickHandler,
+    maintainAspectRatio: false,
+    plugins: {
+      // tooltip: {
+      //   // Tooltip will only receive click events
+      //   events: ["click"],
+      // },
+      datalabels: {
+        // This code is used to display data values
+        anchor: "center",
+        align: "start",
+        color: "black",
+        font: {
+          size: 20,
+        },
       },
     },
-  },
-  interactions: {
-    mode: "point",
-  },
-};
+    interactions: {
+      mode: "point",
+    },
+  };
 
-// console.log(canvasPosition);
+  // console.log(canvasPosition);
 
-export default function MyPieChart(props) {
   console.log("pie chart data: ", props.data);
   console.log(props.data.labels.length);
   console.log(props.data.datasets.length);
