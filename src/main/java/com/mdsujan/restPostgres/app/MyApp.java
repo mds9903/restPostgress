76209bd75f8fc34b69a26a16b27fbd368878f569
mds9903 <mohammed.sujan@nextupl.com>
2023-02-16 12:23:35 +0530

    added swagger ui to the application

--- src/main/java/com/mdsujan/restPostgres/app/MyApp.java
+++ src/main/java/com/mdsujan/restPostgres/app/MyApp.java
@@ -6,12 +6,15 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication // defines it as the main class for running the app
 @ComponentScan("com.mdsujan.restPostgres") // the pkg that contain components
 @EntityScan("com.mdsujan.restPostgres.entity")
 @EnableMongoRepositories("com.mdsujan.restPostgres.repository")
 @EnableCaching
+@EnableWebMvc
+//@EnableSwagger2
 public class MyApp {
     public static void main(String[] args) {
         SpringApplication.run(MyApp.class, args);
